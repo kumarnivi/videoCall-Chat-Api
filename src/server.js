08890041379be46d03ca49885c2719540eab6941
kumarnivi <nivethikashivakumar56@gmail.com>
2025-11-28 10:21:40 +0530

    set Course

--- src/server.js
+++ src/server.js
@@ -17,7 +17,10 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "https://video-call-chat-ui.vercel.app/login",
+      "http://localhost:5173",
+    ],
     credentials: true, // allow frontend to send cookies
   })
 );
