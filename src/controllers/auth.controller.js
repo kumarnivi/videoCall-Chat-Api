7b3d15699f68a4abb7fb47a7ed6c8c3c18b65610
kumarnivi <nivethikashivakumar56@gmail.com>
2025-11-30 17:23:25 +0530

    Change Cookies

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -86,7 +86,8 @@ export async function login(req, res) {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
       sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
+      // secure: process.env.NODE_ENV === "production",
+      secure: "none",
     });
 
     res.status(200).json({ success: true, user });
